415017361ace5dcb3f198a1fc8f930cbb79ee264
Kwe <Kweayon@gmail.com>
2023-06-08 17:12:25 -0500

    Added imaged and orb / emit colors

--- step_01/lib/main.dart
+++ step_01/lib/main.dart
@@ -5,8 +5,8 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:next_gen_ui/title_screen/title_screen.dart';
 import 'package:window_size/window_size.dart';
-import 'styles.dart';
 
 void main() {
   if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
@@ -24,14 +24,7 @@ class NextGenApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.dark,
       darkTheme: ThemeData(brightness: Brightness.dark),
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Insert Next-Generation UI Here...',
-            style: TextStyles.h2,
-          ),
-        ),
-      ),
+      home: const TitleScreen(),
     );
   }
 }
