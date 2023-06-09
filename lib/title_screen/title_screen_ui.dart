026b6d2222d684c37328b45afb878b25c7917500
Kwe <Kweayon@gmail.com>
2023-06-09 16:57:39 -0500

    Moved files

---
+++ lib/title_screen/title_screen_ui.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'package:extra_alignments/extra_alignments.dart';
+
+import 'package:gap/gap.dart';
+
+import '../assets.dart';
+
+import '../common/ui_scaler.dart';
+
+import '../styles.dart';
+
+class TitleScreenUi extends StatelessWidget {
+  const TitleScreenUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 40,
+        horizontal: 50,
+      ),
+      child: Stack(
+        children: <Widget>[
+          // Title Text
+          // TODO: Finish implementing
+          TopLeft(
+              child: UiScaler(
+            alignment: Alignment.topLeft,
+            child: _TitleText(),
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class _TitleText extends StatelessWidget {
+  const _TitleText({super.key});
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Gap(20),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Transform.translate(
+              offset: Offset(-(TextStyles.h1.letterSpacing! * 0.5), 0),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
